60ca618006d1ddd39c9cd3d2113cbb1b6754d298
Jeff <jwagense@ucsc.edu>
2020-01-06 15:00:58 -0800

    added createCanvas Button

--- drawing.js
+++ drawing.js
@@ -1,1 +1,19 @@
-// intialze a 2d array depending on the given canvas dimensions
+// intialze variable for 2d array
+var arr;
+
+function setup() {
+    
+}
+
+// function to create the canvas with the given dimensions
+function newCanvas(){
+    var width = parseInt(document.getElementById('width').value);
+    var height = parseInt(document.getElementById('height').value);
+     
+    createCanvas(width, height)
+    arr = new Array(width)
+    for(var i = 0; i < width; i ++){
+        arr[i] = new Array(height);
+    } 
+
+}
